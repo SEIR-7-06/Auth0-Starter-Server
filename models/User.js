ece4e83d4154c102b7be982986e09bc2f051c4b5
h64 <hhongnr@gmail.com>
2021-09-21 21:38:07 -0700

    Simplified models

--- models/User.js
+++ models/User.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     auth0Id: String,
-    favoriteFoods: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Food'
-    }]
+    favoriteFoods: []
 })
 
 const User = mongoose.model('User', userSchema)
